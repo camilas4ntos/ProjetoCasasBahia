878fb6216ce31d9bb44b30ab2c468d835f3bd4a1
camilas4ntos <camila.s4ntos@gmail.com>
2018-07-02 09:57:25 -0300

    commit Poc Casas Bahia

---
+++ src/CTCBH01/CtCdh.java
@@ -0,0 +1,84 @@
+package CTCBH01;
+
+import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import Telas_cbh.IdentificacaoCbh;
+import Telas_cbh.CadastroCbh;
+import Telas_cbh.HomeCbh;
+
+public class CtCdh {
+
+	private WebDriver driver;
+	private HomeCbh homecbh;
+	private IdentificacaoCbh indentificacaocbh;	
+	private CadastroCbh cadastrocbh;
+	String email = "teste@teste.com.br";	
+	String nomecompleto = "Maria de Jesus";
+	String cpf ="02970157020";
+	String ddd = "11";
+	String dianasc = "07";
+	String mesnasc = "12";
+	String anonasc = "1980";
+	String telefone = "997876543";
+	String telefone2 = "41678765";
+	String senha = "Teste@01";
+	String linkSenha;	
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		homecbh = new HomeCbh();
+		indentificacaocbh = new IdentificacaoCbh();
+		cadastrocbh = new CadastroCbh();
+		
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\yamanadmin\\Documents\\DriveJar\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(2 , TimeUnit.SECONDS);
+		driver.get("https://www.casasbahia.com.br/");
+		
+		homecbh.menuCBH(driver).click();
+		indentificacaocbh.email(driver).sendKeys(email);
+		indentificacaocbh.radiobutton(driver).click();
+//		indentificacaocbh.botaocontinuar(driver).click();
+//		cadastrocbh.nomecompleto(driver).sendKeys(nomecompleto);
+//		cadastrocbh.cpf(driver).sendKeys(cpf);
+//		//cadastrocbh.tipotelefone1(driver).
+//		cadastrocbh.ddd(driver).sendKeys(ddd);
+//		cadastrocbh.telefone(driver).sendKeys(telefone);
+//		cadastrocbh.ddd2(driver).sendKeys(ddd);
+//		cadastrocbh.telefone2(driver).sendKeys(telefone2);
+//		cadastrocbh.dianasc(driver).sendKeys(dianasc);
+//		cadastrocbh.mesnasc(driver).sendKeys(mesnasc);
+//		cadastrocbh.anonasc(driver).sendKeys(anonasc);
+//		cadastrocbh.confirmaemail(driver).sendKeys(email);
+//		driver.findElement(By.id("Senha")).sendKeys("Senha@01");
+//		cadastrocbh.confirmasenha(driver).sendKeys(senha);
+//		cadastrocbh.salvar(driver).click();
+		
+		
+		
+		linkSenha = indentificacaocbh.esquecisenha(driver).getText();
+        assertEquals(linkSenha, "Esqueci minha senha" );
+		
+	}
+
+}
---
+++ src/Telas_cbh/CadastroCbh.java
@@ -0,0 +1,74 @@
+package Telas_cbh;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CadastroCbh {
+
+	public WebElement nomecompleto (WebDriver driver) {
+		return driver.findElement(By.id("NomeCompleto"));
+	}
+
+	public WebElement cpf (WebDriver driver) {
+		return driver.findElement(By.id("Cpf"));
+	}
+	
+	public WebElement tipotelefone1 (WebDriver driver) {
+		return driver.findElement(By.id("TipoFone1"));
+	}
+	
+	public WebElement tipotelefone2 (WebDriver driver) {
+		return driver.findElement(By.id("TipoFone2"));
+	}
+	
+	public WebElement ddd (WebDriver driver) {
+		return driver.findElement(By.id("Telefone1DDDPF"));
+	}
+	
+	public WebElement telefone (WebDriver driver) {
+		return driver.findElement(By.id("Telefone1PF"));
+	}
+	
+	public WebElement ddd2 (WebDriver driver) {
+		return driver.findElement(By.id("Telefone2DDDPF"));
+	}
+	
+	public WebElement telefone2 (WebDriver driver) {
+		return driver.findElement(By.id("Telefone2PF"));
+	}
+	
+	public WebElement dianasc (WebDriver driver) {
+		return driver.findElement(By.id("DataNascimentoDia"));
+	}
+
+	public WebElement mesnasc (WebDriver driver) {
+		return driver.findElement(By.id("DataNascimentoMes"));
+	}
+	
+	public WebElement anonasc (WebDriver driver) {
+		return driver.findElement(By.id("DataNascimentoAno"));
+	}
+	
+	public WebElement sexo (WebDriver driver) {
+		return driver.findElement(By.id("Sexo"));
+	}
+	
+	public WebElement confirmaemail (WebDriver driver) {
+		return driver.findElement(By.id("ConfirmarEmail"));
+	}
+	
+	//public WebElement senha(WebDriver driver) {
+		//return driver.findElement(By.name("Senha"));
+	//}
+	
+	public WebElement confirmasenha (WebDriver driver) {
+		return driver.findElement(By.id("ConfirmarSenha"));
+	}
+	
+	public WebElement salvar (WebDriver driver) {
+		return driver.findElement(By.id("btnClienteSalvar"));
+	}
+
+	
+}
